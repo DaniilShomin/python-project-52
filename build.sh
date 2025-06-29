c3cadce8861115652718d06e56b51dbb7e5fe3cc
DaniilShomin <d.sh.v@yandex.ru>
2025-06-29 17:38:59 +0300

    fix build

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ source $HOME/.local/bin/env
 
 # здесь добавьте все необходимые команды для установки вашего проекта
 # команду установки зависимостей, сборки статики, применения миграций и другие
-make install && make collectstatic && make migrate
+make install && make migrate && make collectstatic 
 
 psql -h dpg-d1cr1emmcj7s73b97lbg-a.oregon-postgres.render.com -U db_task_manager_q2ck_user db_task_manager_q2ck
