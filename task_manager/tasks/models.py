fe4aa280ed67ac860f30b0a29b9ee4690d292384
DaniilShomin <d.sh.v@yandex.ru>
2025-07-04 20:26:39 +0300

    update relationship labels and tasks

--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from task_manager.statuses.models import Statuses
 from task_manager.users.models import Users
+from task_manager.labels.models import Labels
 
 # Create your models here.
 class Tasks(models.Model):
@@ -9,5 +10,5 @@ class Tasks(models.Model):
     status = models.ForeignKey(Statuses, on_delete=models.CASCADE, related_name='status', null=True)
     author = models.ForeignKey(Users, on_delete=models.CASCADE, related_name='author', null=True)
     executor = models.ForeignKey(Users, on_delete=models.CASCADE , related_name='executor', null=True)
-    label = models.CharField(null=True)
+    label = models.ManyToManyField(Labels, related_name='tasks', null=True)
     created_at = models.DateTimeField(auto_now_add=True)
