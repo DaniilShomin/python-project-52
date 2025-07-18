b9174ccecc075284a71317fb477e988f4e88de02
DaniilShomin <d.sh.v@yandex.ru>
2025-07-18 20:43:41 +0300

    update users app

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -1,17 +1,12 @@
 from django.urls import path
 
-from task_manager.users.views import (
-    CreateUserView,
-    DeleteUserView,
-    IndexUserView,
-    UpdateUserView,
-)
+from task_manager.users import views
 
 app_name = "users"
 
 urlpatterns = [
-    path("", IndexUserView.as_view(), name="index"),
-    path("create/", CreateUserView.as_view(), name="create"),
-    path("<int:pk>/update/", UpdateUserView.as_view(), name="update"),
-    path("<int:pk>/delete/", DeleteUserView.as_view(), name="delete"),
+    path("", views.UserListView.as_view(), name="index"),
+    path("create/", views.UserCreateView.as_view(), name="create"),
+    path("<int:pk>/update/", views.UserUpdateView.as_view(), name="update"),
+    path("<int:pk>/delete/", views.UserDeleteView.as_view(), name="delete"),
 ]
