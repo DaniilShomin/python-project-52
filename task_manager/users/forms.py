a6bff0de8f46747b1448a420fd8316765d49df31
DaniilShomin <d.sh.v@yandex.ru>
2025-07-18 21:01:05 +0300

    add password and username verification

--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -35,47 +35,47 @@ class CreateUserForm(UserCreationForm):
             "password": forms.PasswordInput(),
         }
 
-    # def clean(self):
-    #     password = self.cleaned_data.get("password")
-    #     if not password:
-    #         raise forms.ValidationError(_("You must enter a password."))
-    #     confirm_password = self.cleaned_data.get("confirm_password")
-    #     if password != confirm_password:
-    #         raise forms.ValidationError(
-    #             _("The passwords entered do not match.")
-    #         )
-    #     if len(password) < 3:
-    #         raise forms.ValidationError(
-    #             _(
-    #                 "The password you entered is too short. "
-    #                 "It must contain at least 3 characters."
-    #             )
-    #         )
-    #     return self.cleaned_data
+    def clean(self):
+        password = self.cleaned_data.get("password")
+        if not password:
+            raise forms.ValidationError(_("You must enter a password."))
+        confirm_password = self.cleaned_data.get("confirm_password")
+        if password != confirm_password:
+            raise forms.ValidationError(
+                _("The passwords entered do not match.")
+            )
+        if len(password) < 3:
+            raise forms.ValidationError(
+                _(
+                    "The password you entered is too short. "
+                    "It must contain at least 3 characters."
+                )
+            )
+        return self.cleaned_data
 
-    # def clean_username(self):
-    #     username = self.cleaned_data["username"]
-    #     if len(username) > 150:
-    #         raise forms.ValidationError(
-    #             _("Username is too long (maximum 150 characters).")
-    #         )
-    #     if not all(c.isalnum() or c in "@.+-_" for c in username):
-    #         raise forms.ValidationError(
-    #             _(
-    #                 "Please enter a valid username. "
-    #                 "It can only contain letters, numbers and @/./+/-/_ signs."
-    #             )
-    #         )
+    def clean_username(self):
+        username = self.cleaned_data["username"]
+        if len(username) > 150:
+            raise forms.ValidationError(
+                _("Username is too long (maximum 150 characters).")
+            )
+        if not all(c.isalnum() or c in "@.+-_" for c in username):
+            raise forms.ValidationError(
+                _(
+                    "Please enter a valid username. "
+                    "It can only contain letters, numbers and @/./+/-/_ signs."
+                )
+            )
 
-    #     User = get_user_model()
-    #     existing_user = User.objects.filter(username=username).first()
+        User = get_user_model()
+        existing_user = User.objects.filter(username=username).first()
 
-    #     if existing_user:
-    #         if (
-    #             not hasattr(self, "instance")
-    #             or self.instance.pk != existing_user.pk
-    #         ):
-    #             raise forms.ValidationError(
-    #                 _("A user with this name already exists.")
-    #             )
-    #     return username
+        if existing_user:
+            if (
+                not hasattr(self, "instance")
+                or self.instance.pk != existing_user.pk
+            ):
+                raise forms.ValidationError(
+                    _("A user with this name already exists.")
+                )
+        return username
