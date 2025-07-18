c7283a5ee1a9530fdbbbd52a9c446ebd783e7d2a
DaniilShomin <d.sh.v@yandex.ru>
2025-07-18 23:53:18 +0300

    add .env_example

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -33,7 +33,7 @@ DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = [
     "webserver",
-    "python-project-52-2h58.onrender.com",
+    "https://python-project-52-zib0.onrender.com",
     "127.0.0.1",
     "localhost",
 ]
